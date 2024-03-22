a9d07d86e9cb24ac435b54b05879f362a23ec9f5
EmmettHintz <eahintz@colgate.edu>
2024-03-21 20:59:34 -0400

    Add saving of MI scores to CSV files

--- Feature Selection/Rank FS/MI.py
+++ Feature Selection/Rank FS/MI.py
@@ -17,6 +17,7 @@ def calculate_mutual_information(group):
     
     # Combining T0 and T8 samples for MI calculation
     combined_data = pd.concat([t0, t8])
+    
     for index, miRNA in enumerate(miRNAs):
         # MI expects a continuous target, hence we'll use timepoints as binary labels (0 for T0, 1 for T8)
         labels = [0]*len(t0) + [1]*len(t8)
@@ -25,9 +26,13 @@ def calculate_mutual_information(group):
         
     return pd.DataFrame({'miRNA': miRNAs, 'MI_Score': mi_scores})
 
-def perform_mi_analysis():
+def perform_mi_analysis_and_save():
     treatment_mi = calculate_mutual_information(treatment_group)
     control_mi = calculate_mutual_information(control_group)
+    
+    # Save the MI scores to CSV files
+    treatment_mi.to_csv('/Users/emmetthintz/Documents/Computational-Biology/Data/treatment_mi_scores.csv', index=False)
+    control_mi.to_csv('/Users/emmetthintz/Documents/Computational-Biology/Data/control_mi_scores.csv', index=False)
     
     # For demonstration, we will print the top 5 miRNAs based on MI score
     print("Top 5 MI Scores in Treatment Group:")
@@ -35,4 +40,4 @@ def perform_mi_analysis():
     print("\nTop 5 MI Scores in Control Group:")
     print(control_mi.sort_values(by='MI_Score', ascending=False).head())
     
-perform_mi_analysis()
+perform_mi_analysis_and_save()
